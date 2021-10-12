f96180a43ca6e8bbac4c324659d74122809d184b
Anton Heestand <anton.heestand@bontouch.com>
2021-10-12 11:39:54 +0200

    Initial Commit

---
+++ Shared/EuclidDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EuclidDemoApp.swift
+//  Shared
+//
+//  Created by Anton Heestand on 2021-10-12.
+//
+
+import SwiftUI
+
+@main
+struct EuclidDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
