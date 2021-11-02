27ba45015a023f12b78085f49b8d86b1fa2bfc80
Anton Heestand <anton.heestand@bontouch.com>
2021-11-02 19:47:04 +0100

    UI

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,168 @@
 //
 
 import SwiftUI
+import RealityKit
+import Euclid
+import SceneKit
 
 struct ContentView: View {
+    
+    @State var boxEntity: Entity?
+    @State var sphereEntity: Entity?
+    @State var euclidEntity: Entity?
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            HStack {
+                
+                if let entity: Entity = boxEntity {
+                    RealityView(entity: entity)
+                }
+                
+                Text("-")
+                    .font(.system(size: 100, weight: .bold, design: .monospaced))
+                
+                if let entity: Entity = sphereEntity {
+                    RealityView(entity: entity)
+                }
+            }
+            
+            Text("=")
+                .font(.system(size: 100, weight: .bold, design: .monospaced))
+            
+            if let entity: Entity = euclidEntity {
+                RealityView(entity: entity)
+            } else {
+                VStack {
+                    ProgressView()
+                        .padding(20)
+                    Text("Euclid is processing the bool operation.")
+                    Text("This can take a couple minutes with large meshes...")
+                }
+                .padding(50)
+            }
+        }
+        .onAppear {
+            
+            let boxMesh: MeshResource = .generateBox(size: 1.0)
+            let sphereMesh: MeshResource = .generateSphere(radius: 0.75)
+            
+            boxEntity = ModelEntity(mesh: boxMesh)
+            sphereEntity = ModelEntity(mesh: sphereMesh)
+            
+            let boxEuclidMesh: Mesh = toEuclid(mesh: boxMesh)
+            let sphereEuclidMesh: Mesh = toEuclid(mesh: sphereMesh)
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+            
+                print("Euclid Bool...")
+                
+                /// This can take a minute
+                let euclidMesh: Mesh = sphereEuclidMesh.subtract(boxEuclidMesh)
+                
+                print("Euclid Done!")
+
+                DispatchQueue.main.async {
+                    
+                    let finalMesh: MeshResource = try! fromEuclid(mesh: euclidMesh)
+                    
+                    euclidEntity = ModelEntity(mesh: finalMesh)
+                }
+            }
+        }
+    }
+    
+    func toEuclid(mesh: MeshResource) -> Mesh {
+        
+        var inPositions: [SIMD3<Float>] = []
+        var inNormals: [SIMD3<Float>] = []
+        var inIndices: [UInt32] = []
+        var inCoords: [SIMD2<Float>] = []
+
+        for model in mesh.contents.models {
+            for part in model.parts {
+
+                for position in part.positions.elements {
+                    inPositions.append(position)
+                }
+
+                if let normals = part.normals {
+                    for normal in normals.elements {
+                        inNormals.append(normal)
+                    }
+                }
+
+                if let triangles = part.triangleIndices {
+                    for triangleIndex in triangles.elements {
+                        inIndices.append(triangleIndex)
+                    }
+                }
+
+                if let textureCoordinates = part.textureCoordinates {
+                    for textureCoordinate in textureCoordinates.elements {
+                        inCoords.append(textureCoordinate)
+                    }
+                }
+
+            }
+        }
+        
+        var sources: [SCNGeometrySource] = []
+        var elements: [SCNGeometryElement] = []
+        sources.append(SCNGeometrySource(vertices: inPositions.map({ vector in
+            SCNVector3(vector.x, vector.y, vector.x)
+        })))
+        elements.append(SCNGeometryElement(indices: inIndices, primitiveType: .triangles))
+        let geometry = SCNGeometry(sources: sources, elements: elements)
+        
+        return Mesh(geometry, materialLookup: nil)!
+    }
+    
+    func fromEuclid(mesh: Mesh) throws -> MeshResource {
+        
+        var descriptor = MeshDescriptor(name: "test")
+        descriptor.positions = MeshBuffers.Positions(
+            mesh.polygons.flatMap({ polygon in
+                polygon.vertices.map { vertex in
+                    SIMD3<Float>(
+                        x: Float(vertex.position.x),
+                        y: Float(vertex.position.y),
+                        z: Float(vertex.position.z)
+                    )
+                }
+            })
+        )
+        descriptor.normals = MeshBuffers.Normals(
+            mesh.polygons.flatMap({ polygon in
+                polygon.vertices.map { vertex in
+                    SIMD3<Float>(
+                        x: Float(vertex.normal.x),
+                        y: Float(vertex.normal.y),
+                        z: Float(vertex.normal.z)
+                    )
+                }
+            })
+        )
+        descriptor.textureCoordinates = MeshBuffers.TextureCoordinates(
+            mesh.polygons.flatMap({ polygon in
+                polygon.vertices.map { vertex in
+                    SIMD2<Float>(
+                        x: Float(vertex.texcoord.x),
+                        y: Float(vertex.texcoord.y)
+                    )
+                }
+            })
+        )
+        var prims: [UInt32] = []
+        var index: UInt32 = 0
+        for _ in mesh.polygons {
+            prims.append(contentsOf: [index, index + 1, index + 2])
+            index += 3
+        }
+        descriptor.primitives = .triangles(prims)
+        
+        return try .generate(from: [descriptor])
     }
 }
 
---
+++ Shared/RealityView.swift
@@ -0,0 +1,27 @@
+//
+//  RealityView.swift
+//  RealityNodesDemo
+//
+//  Created by Anton Heestand on 2021-10-10.
+//
+
+import SwiftUI
+import MultiViews
+import RealityKit
+
+struct RealityView: ViewRepresentable {
+    
+    let entity: Entity
+    
+    func makeView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero)
+        arView.environment.background = .color(.clear)
+        let anchor = AnchorEntity(.world(transform: matrix_identity_float4x4))
+        anchor.addChild(entity)
+        arView.scene.addAnchor(anchor)
+        return arView
+    }
+    
+    func updateView(_ view: ARView, context: Context) {}
+    
+}
